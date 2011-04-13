b58daef5a539f4bbc18f2650d60f272aa441f5a3
Kaspar Schiess <kaspar.schiess@absurd.li>
2011-04-13 08:45:54 +0200

    + Comment for the service

--- lib/cod/service.rb
+++ lib/cod/service.rb
@@ -1,6 +1,25 @@
 
 module Cod
-  # A service that receives requests and answers. 
+  # A service that receives requests and answers. A service has always at
+  # least one provider that creates an instance of this class and waits in
+  # #one or #each. Clients then instantiate Cod::Client and #call the service.
+  #
+  # Example: 
+  #   # service side
+  #   service = Cod::Service.new(incoming_channel)
+  #   service.one { |msg| 'answer' }
+  # 
+  #   # client side
+  #   client = Cod::Client.new(incoming_channel, service_channel)
+  #   client.call('call message') # => 'answer'
+  #
+  # == Topology
+  # 
+  # A service always has (potentially) multiple clients and depending on the
+  # transport layer used, one or more workers handling the clients request.
+  # They will always receive the messages in a round robin fashion; the
+  # service corresponds in this case to the channel address; clients need not
+  # know the workers involved. 
   #
   class Service
     # Incoming channel for requests.
