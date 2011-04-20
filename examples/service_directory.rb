b9892f655e08453f1ffb5c6e0be5c1cb03b6f409
Kaspar Schiess <kaspar.schiess@absurd.li>
2011-04-20 08:03:46 +0200

    . mo' comments

--- examples/service_directory.rb
+++ examples/service_directory.rb
@@ -6,7 +6,11 @@ directory = Cod::Directory.new(announce)
 
 pipes = []
 pids = ['foo.bar', /^foo\..+/].map { |match_expr| 
+  # Creates a communication channel that both the parent and the child know
+  # about. After the fork, they will own unique ends to that channel. 
   pipes << pipe = Cod.pipe  # store in pipes as well to prevent GC
+  
+  # Create a child that will receive messages that match match_expr.
   fork do
     puts "Spawned child: #{Process.pid}"
     topic = Cod::Topic.new(match_expr, announce, pipe)
@@ -20,9 +24,9 @@ pids = ['foo.bar', /^foo\..+/].map { |match_expr|
     end
   end }
   
-directory.publish 'foo.bar', 'Hi everyone!'
-directory.publish 'foo.baz', 'Hi you!'
-directory.publish 'no.one', 'echo?'
+directory.publish 'foo.bar', 'Hi everyone!'   # to both childs
+directory.publish 'foo.baz', 'Hi you!'        # only second child matches this
+directory.publish 'no.one', 'echo?'           # no one matches this
 
-directory.publish 'foo.bar', :shutdown  
+directory.publish 'foo.bar', :shutdown        # shutdown children in orderly fashion
 Process.waitall
