f32b904e4d004eb0f37bc985da35342c74b382ef
Kaspar Schiess <kaspar.schiess@absurd.li>
2011-06-08 20:48:08 +0200

    - useless spec, implementors are now more free

--- spec/lib/cod/channel/base_spec.rb
+++ spec/lib/cod/channel/base_spec.rb
@@ -3,32 +3,4 @@ require 'spec_helper'
 describe Cod::Channel::Base do
   let(:base) { described_class.new }
   
-  describe "#_dump" do
-    after(:each) { Thread.current[:cod_serializing_channel] = nil }
-    
-    it "should ask the serializer for permission when serializing channels" do
-      serializer = Thread.current[:cod_serializing_channel] = 
-        flexmock(:serializer)
-
-      serializer.
-        should_receive(:may_transmit?).with(base).and_return(false)
-        
-      expect {
-        base._dump(-1)
-      }.to raise_error(Cod::Channel::CommunicationError)
-    end 
-  end
-  describe "._load(str)" do
-    after(:each) { Thread.current[:cod_deserializing_channel] = nil }
-    
-    it "should permit the deserializer to transform channels in the message" do
-      deserializer = Thread.current[:cod_deserializing_channel] = 
-        flexmock(:deserializer)
-        
-      deserializer.
-        should_receive(:replaces).with(:wire_str).and_return(:replaced_channel)
-      
-      described_class._load(Marshal.dump(:wire_str))
-    end 
-  end
 end
