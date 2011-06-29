6174376b4d1d73f9b355c89d6ef332996de7a5f1
Kaspar Schiess <kaspar.schiess@absurd.li>
2011-06-29 07:34:09 +0200

    ! fixed example to use factory methods

--- examples/pong.rb
+++ examples/pong.rb
@@ -1,7 +1,7 @@
 $:.unshift File.expand_path(File.dirname(__FILE__) + "/../lib")
 require 'cod'
 
-pipe = Cod::Channel::Beanstalk.new('localhost:11300', "pingpong")
+pipe = Cod.beanstalk('localhost:11300', "pingpong")
 
 loop do
   puts "Received: "+pipe.get.inspect
