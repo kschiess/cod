0167f0b197065508d01157c0606960e70c9758eb
Kaspar Schiess <kaspar.schiess@absurd.li>
2011-07-05 07:14:11 +0200

    . documentation

--- lib/cod.rb
+++ lib/cod.rb
@@ -1,6 +1,26 @@
 require 'at_fork'
 
-# See Cod::Channel::Base for more documentation. 
+# The core concept of Cod are 'channels'. (Cod::Channel::Base) You can create
+# such channels on top of the various transport layers. Once you have such a
+# channel, you #put messages into it and you #get messages out of it. Messages
+# are retrieved in FIFO manner, making channels look like a communication pipe
+# most of the time. 
+#
+# Cod also brings a few abstractions layered on top of channels: You can use
+# channels to present 'services' (Cod::Service) to the network: A service is a
+# simple one or two way RPC call. (one way = asynchronous) You can also use
+# channels to run a 'directory' (Cod::Directory) where processes subscribe to
+# information using a filter. They then get information that matches their
+# filter written to their inbound channel. (also called pub/sub)
+#
+# The goal of Cod is that you have to know only very few things about the
+# network (the various transports) to be able to construct complex things. It
+# handles reconnection and reliability for you. It also translates cryptic OS
+# errors into plain text messages where it can't just handle them. This should
+# give you a clear place to look at if things go wrong. Note that this can
+# only be ever as good as the sum of situations Cod has been tested in.
+# Contribute your observations and we'll come up with a way of dealing with
+# most of the tricky stuff!
 #
 module Cod
   # This gets raised in #create_reference when the identifier passed in is
