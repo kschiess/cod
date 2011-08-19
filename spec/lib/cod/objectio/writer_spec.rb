7da0d9a89227e53228e01d45ff5ef6960009f4c4
Kaspar Schiess <kaspar.schiess@absurd.li>
2011-08-19 15:16:34 +0200

    - old spec that now doesn't help

--- spec/lib/cod/objectio/writer_spec.rb
+++ spec/lib/cod/objectio/writer_spec.rb
@@ -1,24 +1,4 @@
 require 'spec_helper'
 
 describe Cod::ObjectIO::Writer do
-  let(:connection_factory) { flexmock(:connection_factory) }
-  let(:serializer) { flexmock(:serializer, :serialize => '.') }
-
-  before(:each) { connection_factory.should_receive(:do).by_default }
-  
-  let(:writer) { described_class.new(serializer) { connection_factory.do } }
-  
-  context "if a connection can't be made" do
-    it "looses the message" do
-      writer.put 'message'
-    end
-    it "attempts reconnect before every send" do
-      io = flexmock(:io)
-
-      connection_factory.should_receive(:do => io).once
-      io.should_receive(:write).with('.').once
-
-      writer.put 'message'
-    end
-  end
 end
