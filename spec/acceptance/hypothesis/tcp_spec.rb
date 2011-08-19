06bed622c4e31b62579f53f6bb481083daedfb66
Kaspar Schiess <kaspar.schiess@absurd.li>
2011-08-19 11:14:32 +0200

    . inverse hypothesis also holds

--- spec/acceptance/hypothesis/tcp_spec.rb
+++ spec/acceptance/hypothesis/tcp_spec.rb
@@ -1,12 +1,16 @@
 require 'spec_helper'
 
 describe "TCP" do
+  let!(:server) { TCPServer.new('127.0.0.1', 33000) }
+  let!(:client) { TCPSocket.new('127.0.0.1', 33000) }
+  let(:server_end) { server.accept }
+  
+  after(:each) { 
+    server.close rescue nil
+    client.close rescue nil
+  }
+  
   it "doesn't fail when sending data to a closed socket" do
-    server = TCPServer.new('127.0.0.1', 33000)
-    client = TCPSocket.new('127.0.0.1', 33000)
-    
-    server_end = server.accept
-    
     client.write('.')
     begin
       server_end.read_nonblock(100).should == '.'
@@ -29,5 +33,27 @@ describe "TCP" do
     ready = IO.select([client], [client], [client])
     ready[0].should have(1).element
     ready[1].should have(1).element
+  end 
+  it "detects closing of a client socket when reading from it" do
+    client.write('.')
+    begin
+      server_end.read_nonblock(100).should == '.'
+    rescue Errno::EAGAIN
+      retry
+    end
+    client.close
+
+    # IO select doesn't know about closed client ends.
+    ready = IO.select([server_end], [server_end], [server_end])
+    ready[0].should have(1).element
+    ready[1].should have(1).element
+    
+    # Writing doesn't fail.
+    server_end.write('.')
+    
+    # But reading always should
+    expect {
+      server_end.read_nonblock(1)
+    }.to raise_error # either Errno::ECONNRESET or EOFError
   end 
 end
