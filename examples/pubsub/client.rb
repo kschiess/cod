4ef774a8810d10e257b0d61e19a9924455b991ac
Kaspar Schiess <kaspar.schiess@absurd.li>
2011-08-25 09:18:26 +0200

    . documentation

--- examples/pubsub/client.rb
+++ examples/pubsub/client.rb
@@ -5,7 +5,7 @@ channels = Struct.new(:directory, :answers).new(
   Cod.beanstalk('localhost:11300', 'directory'), 
   Cod.beanstalk('localhost:11300', 'directory.'+Cod.uuid))
   
-topic = Cod::Topic.new('', channels.directory, channels.answers)
+topic = Cod::Topic.new('', channels.directory, channels.answers, :renew => 5)
 
 loop do
   puts topic.get
