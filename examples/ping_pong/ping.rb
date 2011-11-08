f12784bf928d5ca7b0f6378fa7431ce1f2f04f1f
Kaspar Schiess <kaspar.schiess@absurd.li>
2011-11-08 10:45:04 +0100

    + examples cleanup

--- examples/ping_pong/ping.rb
+++ examples/ping_pong/ping.rb
@@ -1,4 +1,4 @@
-$:.unshift File.expand_path(File.dirname(__FILE__) + "/../lib")
+$:.unshift File.expand_path(File.dirname(__FILE__) + "/../../lib")
 require 'cod'
 
 pipe = Cod.beanstalk('localhost:11300', 'pingpong')
