54e86b558b75142b5a902f1478feb732b273a8d7
Florian Hanke <florian.hanke@gmail.com>
2011-11-24 23:20:05 +1100

    + simplified regexp matching

--- lib/cod/beanstalk/serializer.rb
+++ lib/cod/beanstalk/serializer.rb
@@ -1,20 +1,20 @@
 module Cod::Beanstalk
   # This is a kind of beanstalk message middleware: It generates and parses
   # beanstalk messages from a ruby format into raw bytes. The raw bytes go
-  # directly into the tcp channel that underlies the beanstalk channel. 
+  # directly into the tcp channel that underlies the beanstalk channel.
   #
   # Messages are represented as simple Ruby arrays, specifying first the
-  # beanstalkd command, then arguments. Examples: 
+  # beanstalkd command, then arguments. Examples:
   #   [:use, 'a_tube']
   #   [:delete, 123]
   #
   # One exception: The commands that have a body attached will be described
-  # like so in protocol.txt: 
+  # like so in protocol.txt:
   #   put <pri> <delay> <ttr> <bytes>\r\n
   #   <data>\r\n
   #
   # To generate this message, just put the data where the bytes would be and
-  # the serializer will do the right thing. 
+  # the serializer will do the right thing.
   #   [:put, pri, delay, ttr, "my_small_data"]
   #
   # Results come back in the same way, except that the answers take the place
@@ -25,7 +25,7 @@ module Cod::Beanstalk
   class Serializer
     def en(msg)
       cmd = msg.first
-      
+
       if cmd == :put
         body = msg.last
         format(*msg[0..-2], body.bytesize) << format(body)
@@ -33,11 +33,11 @@ module Cod::Beanstalk
         format(*msg)
       end
     end
-    
+
     def de(io)
       str = io.gets("\r\n")
       raw = str.split
-      
+
       cmd = convert_cmd(raw.first)
       msg = [cmd, *convert_args(raw[1..-1])]
 
@@ -49,32 +49,32 @@ module Cod::Beanstalk
         fail "No crlf at end of data?" unless data[-2..-1] == "\r\n"
         msg[-1] = data[0..-3]
       end
-      
+
       msg
     end
-    
+
   private
     # Joins the arguments with a space and appends a \r\n
     #
     def format(*args)
       args.join(' ') << "\r\n"
     end
-    
+
     # Converts a beanstalkd answer like INSERTED to :inserted
-    # 
+    #
     def convert_cmd(cmd)
       cmd.downcase.to_sym
     end
-    
+
     # Converts an argument to either a number or a string, depending on
-    # what it looks like. 
-    # 
+    # what it looks like.
+    #
     # Example:
     #   convert_args(['1', 'a string']) # => [1, 'a string']
     #
     def convert_args(args)
-      args.map { |e| 
-        /^\d+$/.match(e) ? Integer(e) : e }
+      args.map { |e|
+        /^\d+$/ =~ e ? Integer(e) : e }
     end
   end
 end
