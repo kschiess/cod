148cb999d55ad3dcc2313247d39034af0735397d
Kaspar Schiess <kaspar.schiess@absurd.li>
2011-11-28 12:14:07 +0100

    + killing the service makes this more reliable

--- spec/acceptance/service_spec.rb
+++ spec/acceptance/service_spec.rb
@@ -12,7 +12,7 @@ describe "Cod::Service" do
 
       block.call(server)
     end
-    self.class.after(:each) { Process.wait(pid) }
+    self.class.after(:each) { Process.kill('KILL', pid); Process.wait(pid) }
   end
 
   [
