dc90de737e69c68156ee9f93a045a1feeb3e8d7d
Kaspar Schiess <kaspar.schiess@absurd.li>
2012-02-27 16:01:45 +0100

    + Cod::ConnectionLost should be raised when the connection goes down.
    This allows simply reconnecting on error, see examples/bs_ping_pong/{ping,pong}.rb
    on how to do this.

--- examples/bs_ping_pong/ping.rb
+++ examples/bs_ping_pong/ping.rb
@@ -1,9 +1,15 @@
 $:.unshift File.expand_path(File.dirname(__FILE__) + "/../../lib")
 require 'cod'
 
-pipe = Cod.beanstalk('pingpong', 'localhost:11300')
+begin
+  pipe ||= Cod.beanstalk('pingpong', 'localhost:11300')
 
-loop do
-  pipe.put Time.now
-  sleep 1
+  loop do
+    pipe.put Time.now
+    sleep 1
+  end
+rescue Cod::ConnectionLost
+  pipe.close
+  pipe = nil
+  retry
 end
--- examples/bs_ping_pong/pong.rb
+++ examples/bs_ping_pong/pong.rb
@@ -1,9 +1,13 @@
 $:.unshift File.expand_path(File.dirname(__FILE__) + "/../../lib")
 require 'cod'
 
-pipe = Cod.beanstalk("pingpong", 'localhost:11300')
+begin
+  pipe ||= Cod.beanstalk("pingpong", 'localhost:11300')
 
-loop do
-  puts "Received: "+pipe.get.inspect
-end
-  
+  loop do
+    puts "Received: "+pipe.get.inspect
+  end
+rescue Cod::ConnectionLost
+  pipe.close; pipe = nil
+  retry
+end  
