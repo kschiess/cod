2056e00eaab613c414c59cb8199ecf16f6a9fedb
Kaspar Schiess <kaspar.schiess@absurd.li>
2012-02-27 11:24:40 +0100

    ! Fixes example, missing require

--- examples/bs_ping_pong/ping.rb
+++ examples/bs_ping_pong/ping.rb
@@ -1,7 +1,7 @@
 $:.unshift File.expand_path(File.dirname(__FILE__) + "/../../lib")
 require 'cod'
 
-pipe = Cod.beanstalk('localhost:11300', 'pingpong')
+pipe = Cod.beanstalk('pingpong', 'localhost:11300')
 
 loop do
   pipe.put Time.now
