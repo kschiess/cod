a55bc7db56a83f858ec65d9d630c5fe4a468766e
Kaspar Schiess <kaspar.schiess@absurd.li>
2012-03-27 08:09:44 +0200

    + pre and post processing

--- tutorial/example_runner.rb
+++ tutorial/example_runner.rb
@@ -1,8 +1,11 @@
+
+require 'tempfile'
 require 'case'
 
 class ExampleRunner
   def initialize
     @state = :outside
+    reset_example
   end
   
   def consume(line)
@@ -10,26 +13,70 @@ class ExampleRunner
     any = Case::Any
     
     case [@state, line]
-      when a[:outside, /<pre><code class="sh_ruby">/]
+      when a[:outside, /<pre><code class="sh_ruby" title="(.*)">/]
+        extract_title(line)
         enter :inside
       when a[:inside, %r(</code></pre>)]
-        puts
         enter :outside
       when a[:inside, any]
-        puts "Example line: #{line}"
+        @current_example << line
     else
       # do nothing
     end
   end
   
+  def extract_title(line)
+    if md=line.match(/title="(.*)"/)
+      @title = md[1]
+    end
+  end
+  
   def enter(new_state)
+    a = lambda { |*args| Case::Array[*args] }
+
+    case [@state, new_state]
+      when a[:inside, :outside]
+        run_example
+        reset_example
+    end
+    
     @state = new_state
   end
   
   def run(args)
     Dir['*.textile'].each do |name|
       File.readlines(name).each { |line|
-        consume(line) }
+        consume(line.chomp) }
+    end
+  end
+  
+  def reset_example
+    @title = nil
+    @current_example = []
+  end
+  def run_example
+    return unless @title
+    unless @current_example.grep(/# =>/)
+      puts "Skipping #{@title}, doesn't contain inspection points."
+    end
+
+    print "Running '#@title'..."
+    tempfile = Tempfile.new('exrun')
+    root = File.expand_path(File.dirname(__FILE__))
+    begin
+      tempfile.puts "$:.unshift #{root.inspect}"
+      tempfile.puts "load 'prelude.rb'"
+      tempfile.write(@current_example.join("\n"))
+      tempfile.puts "\nload 'postscriptum.rb'"
+      tempfile.close(false)
+      
+      # puts File.read(tempfile.path)
+    
+      pid = Process.spawn("ruby #{tempfile.path}")
+      Process.wait(pid)
+      puts 'done.'
+    ensure 
+      tempfile.close(true)
     end
   end
 end
