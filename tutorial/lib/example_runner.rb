92fd03033419d6949f3f6a3768c4d2a8f2afd09e
Kaspar Schiess <kaspar.schiess@absurd.li>
2012-03-28 17:01:11 +0200

    Auto-update docs.

---
+++ tutorial/lib/example_runner.rb
@@ -0,0 +1,83 @@
+
+require 'case'
+require 'text/highlight'
+
+require 'example'
+
+class ExampleRunner
+  def initialize
+    @state = :outside
+    @line_count = 0
+  end
+  
+  def consume(line, file)
+    @line_count += 1
+    
+    a = lambda { |*args| Case::Array[*args] }
+    any = Case::Any
+    
+    case [@state, line]
+      when a[:outside, /<pre class="sh_ruby"><code title="(.*)">/]
+        extract_title(line, file)
+        enter :inside
+      when a[:inside, %r(</code></pre>)]
+        enter :outside
+      when a[:inside, any]
+        @example << line
+    else
+      # do nothing
+    end
+  end
+  
+  def extract_title(line, file)
+    if md=line.match(/title="(.*)"/)
+      @example = Example.new(md[1], file, @line_count)
+    end
+  end
+  
+  def enter(new_state)
+    a = lambda { |*args| Case::Array[*args] }
+
+    case [@state, new_state]
+      when a[:inside, :outside]
+        run_example
+    end
+    
+    @state = new_state
+  end
+  
+  def run(args)
+    Dir['*.textile'].each do |name|
+      File.readlines(name).each { |line|
+        consume(line.chomp, name) }
+    end
+  end
+  
+  def run_example
+    String.highlighter = Text::ANSIHighlighter.new
+    print "Running #@example... "
+    
+    if @example.skip?
+      puts "Skipped, no inspection points."
+      return
+    end
+    
+    unless @example.run
+      puts "error".red
+      @example.output[:err].lines.each { |line| 
+        print "  " + line.magenta }
+      return
+    end
+    
+    puts 'ok.'.green
+    
+    @example.check_expectations
+    puts @example.produce_modified_code
+    
+    @example = nil
+  end
+end
+
+if $0 == __FILE__
+  ExampleRunner.new.run(ARGV)
+end
